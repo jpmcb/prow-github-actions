1552ac9e5c15e1fa832f1bdfead6002028d77e01
John McBride <jmcbride@pivotal.io>
2020-04-12 15:47:00 -0600

    WIP: getting self assignment working

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,28 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+  
+    const token = core.getInput("github-token");
+    const command = core.getInput("command", { required: true })
+    
+    core.debug(`command: ${command}`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const issueNumber: number | undefined = github.context.payload.issue?.number
+    const commentBody: string = github.context.payload["comment"]["body"]
+    const commenterId: string = github.context.payload["user"]["id"]
 
-    core.setOutput('time', new Date().toTimeString())
+    if (commentBody.includes(command)) {
+      const octokit = new github.GitHub(token)
+
+      await octokit.issues.addAssignees({
+        ...github.context.repo!,
+        'issue_number': issueNumber!,
+        'assignees': [commenterId]
+      })
+    }
+
   } catch (error) {
     core.setFailed(error.message)
   }
