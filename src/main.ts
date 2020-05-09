e740d7f20cf76f75f8dfc79f5bd45840503fbba7
John McBride <jpmmcbride@gmail.com>
2020-05-09 15:21:31 -0600

    Include pull_req event in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import {handleIssueComment} from './issueComment/handleIssueComment'
+import {handlePullReq} from './pullReq/handlePullReq'
 
 async function run(): Promise<void> {
   try {
     switch (github.context.eventName) {
       case 'issue_comment':
         handleIssueComment()
+        break
+
+      case 'pull_request':
+        handlePullReq()
         break
 
       default:
